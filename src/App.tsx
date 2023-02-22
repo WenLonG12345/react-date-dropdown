4e61dd95a762cbc1cab019c3307de0d786c21f0c
Teo Wen Long <teowenlong0316@gmail.com>
2023-02-22 14:41:16 +0800

    feat: add example

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
 import "./App.css";
-import DateDropdownPicker, {YearPicker, MonthPicker, DayPicker} from "react-date-dropdown-picker";
+import DateDropdownPicker, {
+  YearPicker,
+  MonthPicker,
+  DayPicker,
+} from "react-date-dropdown-picker";
 
 function App() {
   return (
-    <div className="App">
-      <DateDropdownPicker
-        onDateChange={() => {}}
-        defaultDate="1997-01-30"
-        yearStart={1996}
-        yearEnd={2000}
-      />
-      <YearPicker
-        onYearChange={() => {}}
-      />
+    <div className="main-container">
+      <div className="pageTitle">Welcome To</div>
+      <div className="pageTitle" style={{ marginBottom: "20px" }}>
+        react-date-dropdown-picker
+      </div>
+      <div className="date-container">
+        <div className="row">
+          <div>Date Dropdown Picker</div>
+          <DateDropdownPicker
+            onDateChange={() => {}}
+            defaultDate="1997-01-30"
+            yearStart={1996}
+            yearEnd={2000}
+          />
+        </div>
+        <div className="row">
+          <div>Year Picker</div>
+          <YearPicker onYearChange={() => {}} />
+        </div>
+        <div className="row">
+          <div>Month Picker</div>
+          <MonthPicker onMonthChange={() => {}} />
+        </div>
+        <div className="row">
+          <div>Day Picker</div>
+          <DayPicker onDayChange={() => {}} />
+        </div>
+      </div>
     </div>
   );
 }
