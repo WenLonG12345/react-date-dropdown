408967262d0ceb3f3f00834edfee186c39b92237
Teo Wen Long <wenlong.teo@qumonintelligence.com>
2023-05-29 22:45:33 +0800

    feat: add external to rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,39 +1,40 @@
 /* eslint-disable import/no-anonymous-default-export */
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import dts from 'rollup-plugin-dts';
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
 
-import packageJson from './package.json' assert { type: 'json' };
+import packageJson from "./package.json" assert { type: "json" };
 
 export default [
   {
-    input: 'src/index.ts',
+    input: "src/index.ts",
     output: [
       {
         file: packageJson.main,
-        format: 'cjs'
+        format: "cjs",
       },
       {
         file: packageJson.module,
-        format: 'esm'
-      }
+        format: "esm",
+      },
     ],
     plugins: [
       resolve(),
       commonjs(),
       typescript({
-        tsconfig: './tsconfig.json',
+        tsconfig: "./tsconfig.json",
         // exclude: [
         //   'src/components/**/*.stories.tsx',
         //   './webpack.config.ts'
         // ]
-      })
-    ]
+      }),
+    ],
+    external: ["react", "react-dom"],
   },
   {
-    input: 'dist/esm/index.d.ts',
-    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-    plugins: [dts()]
-  }
+    input: "dist/esm/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
 ];
