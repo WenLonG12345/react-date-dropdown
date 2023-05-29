cb0fd6aa9399c5c1bc2c9f84ae0c6d8775b4b5b2
Teo Wen Long <wenlong.teo@qumonintelligence.com>
2023-05-29 22:18:08 +0800

    feat: add rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,39 @@
+/* eslint-disable import/no-anonymous-default-export */
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+
+import packageJson from './package.json' assert { type: 'json' };
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs'
+      },
+      {
+        file: packageJson.module,
+        format: 'esm'
+      }
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({
+        tsconfig: './tsconfig.json',
+        // exclude: [
+        //   'src/components/**/*.stories.tsx',
+        //   './webpack.config.ts'
+        // ]
+      })
+    ]
+  },
+  {
+    input: 'dist/esm/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()]
+  }
+];
