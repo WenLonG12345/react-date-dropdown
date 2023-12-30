0e007c65cdc761f06001c4ed6bcc3ab80d47047a
Teo Wen Long <teowenlong0316@gmail.com>
2023-12-30 23:09:59 +0800

    feat: change rollup and use vite for bundling, remove unused packages

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,29 @@
-import type { StorybookConfig } from "@storybook/react-webpack5";
+import type { StorybookConfig } from '@storybook/react-vite';
+
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
+    '@storybook/blocks',
+    '@storybook/addon-styling',
   ],
   framework: {
-    name: "@storybook/react-webpack5",
+    name: '@storybook/react-vite',
     options: {},
   },
   docs: {
-    autodocs: true,
+    autodocs: 'tag',
+  },
+  viteFinal: async (config) => {
+    config.optimizeDeps = {
+      ...config.optimizeDeps,
+      include: ['@storybook/addon-styling', '@storybook/blocks'],
+    };
+
+    return config;
   },
 };
 export default config;
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'react-date-dropdown',
+      fileName: 'react-date-dropdown',
+    },
+  },
+  plugins: [dts(), react()],
+});
